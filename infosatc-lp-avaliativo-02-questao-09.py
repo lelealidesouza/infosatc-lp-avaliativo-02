f94a297b04545654f03baa597e0cc767202ee889
lelealidesouza <lelealidesouza@gmail.com>
2021-09-09 16:10:03 -0300

    infosatc-lp-avaliativo-02-questao-09

---
+++ infosatc-lp-avaliativo-02-questao-09.py
@@ -0,0 +1,8 @@
+num1 = int(input("Um numero inteiro: "))
+num2 = int(input("Um numero inteiro: "))
+num3 = int(input("Um numero inteiro: "))
+num4 = int(input("Um numero inteiro: "))
+num5 = int(input("Um numero inteiro: "))
+lista = [num1, num2, num3, num4, num5]
+print(lista)
+print("Em ordem numerica ficara: ", sorted(lista))
