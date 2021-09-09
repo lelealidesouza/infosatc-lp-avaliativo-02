f9d2297a1d519a5c954ebf3832b2ad9e1a30f5db
lelealidesouza <lelealidesouza@gmail.com>
2021-09-09 16:05:17 -0300

    infosatc-lp-avaliativo-02-questao-06

---
+++ infosatc-lp-avaliativo-02-questao-06.py
@@ -0,0 +1,5 @@
+elemento = input("Digite algo da lista para verificar sua posição: ")
+
+lista = ["vento", "fogo", "pedra", "agua"]
+
+print("A posição de {} é {}".format(elemento, lista.index(elemento)))
